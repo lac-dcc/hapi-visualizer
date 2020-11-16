21ce267fa8a9053e1e3bf930539119c19a54339f
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-11-15 21:41:55 -0300

    chore(frontend): drag an drop files woring

--- static/new/index.js
+++ static/new/index.js
@@ -32,8 +32,6 @@ function genDot(svgId, dot) {
 
 $(document).ready(function () {
 
-  $("textarea#yaml").val("");
-
   $("#generate").click(function (e) {
 
     const hapi = $('textarea#hapi').val();
@@ -59,11 +57,107 @@ $(document).ready(function () {
     e.preventDefault();
   });
 
-  $("#verify").click(function (e) {
-    alert("WIP");
-    e.preventDefault();
+  $("#customFile").on('change', function() {
+    var inputedFiles = document.getElementById('customFile').files;
+    addFiles(inputedFiles.files);
   });
 });
 
+function removeFile(removedLi){
+  console.log(removedLi)
+  // get index to remove
+  var index = parseInt(removedLi.getAttribute('index'));
+  
+  // remove the indexed element from the <ul>
+  var ul = removedLi.parentElement;
+  ul.removeChild(removedLi);
 
+  //apply FileForUp.splice(i, 1)
+  filesForUp.splice(index, 1);
 
+  //reset the attr (index, id) for each <li>
+  var childrens = ul.children;
+  for(i=0; i< childrens.length; i++){
+    childrens[i].setAttribute('index', i)
+  }
+
+}
+
+var filesForUp = [];
+var maxFiles = 5;
+function addFiles(files){
+  var size = files.length;
+  if(size + filesForUp > maxFiles){
+    // put a message to show maxFiles overflow
+    return false;
+  }
+
+  var ul = document.getElementById('upload-list');
+
+  for(var i=0; i<size; i++){
+    var li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center'
+    
+    var span = document.createElement('span');
+    span.appendChild(document.createTextNode(files[i].name));
+    li.appendChild(span);
+    
+    var button = document.createElement('button');
+    button.addEventListener('click', function(ev){
+      removeFile(ev.target.parentElement);
+    });
+    button.className = 'btn btn-danger btn-sm'
+    button.appendChild(document.createTextNode('X'));
+    button.setAttribute('type', 'button'); // added line
+    li.appendChild(button);
+    
+    
+    var elemNumber = filesForUp.length+i;
+    // li.setAttribute('id', 'file'+elemNumber); // added line
+    li.setAttribute('index', elemNumber); // added line
+    ul.appendChild(li);
+    filesForUp.push(files[i])
+  }
+}
+
++ function($) {
+  'use strict';
+
+  // UPLOAD CLASS DEFINITION
+  // ======================
+
+  var dropZone = document.getElementById('drop-zone');
+
+  dropZone.ondrop = function(e) {
+    // e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
+    this.className = 'col-11  upload-drop-zone';
+    $('#label-drop')[0].className = 'col-auto';
+    addFiles(e.dataTransfer.files)
+  }
+
+  dropZone.ondragover = preventDrag;
+
+  dropZone.ondragleave = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.className = 'col-11 upload-drop-zone';
+    $('#label-drop')[0].className = 'col-auto';
+    return false;
+  }
+    
+  dropZone.ondragenter = preventDrag;
+
+  dropZone.ondragstart = preventDrag;
+}(jQuery);
+
+function preventDrag(e){
+  e.stopPropagation();
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  this.className = 'col-11 upload-drop-zone drop';
+
+  $('#label-drop')[0].className = 'col-auto label-blue';
+  return false;
+}
